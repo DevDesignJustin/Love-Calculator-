4037fb85f7b9f2aa79800445c4ccd0628d063192
Justin <justinmk119@gmail.com>
2023-03-29 20:44:40 -0400

    Update AI

--- script.js
+++ script.js
@@ -19,12 +19,16 @@ function runCalculation() {
     alert(`${p1} and ${p2} love ${randomNumber}%`);
   }
   checkNames(names, 'Justin', 'Evie', 90, 100, 1);
-  checkNames(names, 'Justin', 'Christina', 80, 100, 2);
+  checkNames(names, 'Justin', 'Christina', 80, 90, 2);
   checkNames(names, 'Avi', 'Devina', 80, 100, 3);
   checkNames(names, 'Caden', 'Izzy', 90, 100, 4);
-  checkNames(names, 'Quinn', 'Marco', 80, 100, 5);
-  checkNames(names, 'Lorenzo', 'Marco', 80, 100, 6);
+  checkNames(names, 'Lorenzo', 'Izzy', 60, 70, 5);
+  checkNames(names, 'Quinn', 'Marco', 80, 100, 6);
+  checkNames(names, 'Lorenzo', 'Marco', 80, 100, 7);
+  checkNames(names, 'Caden', 'Ryan Reynolds', 99, 100, 8);
 
-  checkNames(names, 'Avi', 'Kimaya', 0, 30, 7);
+  checkNames(names, 'Avi', 'Kimaya', 0, 20,9);
+  checkNames(names, 'James', 'Izzy', 0, 30, 10);
+  checkNames(names, 'Will', 'Izzy', 0, 30, 111);
   checkNames(names, p1, p2, 0, 100, 8);
 }
